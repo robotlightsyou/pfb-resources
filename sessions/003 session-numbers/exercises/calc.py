4593fe2c2409a9745cd51667e56cdee9611592c2
robotlightsyou <robotlightsyou@gmail.com>
2021-04-20 12:33:09 -0700

    migrate for old repo

---
+++ sessions/003 session-numbers/exercises/calc.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+
+"""
+This program is a basic calculator providing the user addition,
+subtraction, multiplication, float division, integer division,
+and the modulo operation.
+"""
+
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mult(a, b):
+    return a * b
+
+
+def fdiv(a, b):
+    return a / b
+
+
+def idiv(a, b):
+    return a // b
+
+
+def mod(a, b):
+    return a % b
+
+def exp(a, b):
+    return a ** b
+
